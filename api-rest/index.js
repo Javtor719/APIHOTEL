9920ae168a182904e0ed7eb99fcc6f0898b4f5fd
miguel-am <migagumor@alu.edu.gva.es>
2026-01-11 19:28:02 +0100

    feat: Rutas Añadidas y archivos de rutas basicos

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,7 +1,15 @@
 const express = require ('express');
 const mongoose = require('mongoose');
+const reservaRoutes = require ('./routes/reservaRoutes');
+const usuarioRoutes = require ('./routes/usuarioRoutes');
+const habitacionRoutes = require ('./routes/habitacionRoutes');
 
 const app = express();
+
+app.use('/reserva',reservaRoutes);
+app.use('/usuario',usuarioRoutes);
+app.use('/habitacion',habitacionRoutes);
+
 
 mongoose.connect('mongodb://localhost:27017/hotel_pere_maria')
   .then(() => console.log('Conectado a MongoDB'))
