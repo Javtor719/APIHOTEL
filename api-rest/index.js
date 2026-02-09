92ad126b90167808d6dcd5a024bb299425b5a257
miguel-am <migagumor@alu.edu.gva.es>
2026-02-09 21:06:13 +0100

    feat: Modelo vista controlador users creado

--- api-rest/index.js
+++ api-rest/index.js
@@ -1,14 +1,19 @@
 const express = require ('express');
 const mongoose = require('mongoose');
 const reservationRoutes = require ('./routes/reservationRoutes');
-const usuarioRoutes = require ('./routes/usuarioRoutes');
+const usersRoutes = require ('./routes/userRoutes');
 const habitacionRoutes = require ('./routes/habitacionRoutes');
+const reviewRoutes = require ('./routes/reviewRoutes');
+const  authRouter  = require('./routes/authRouter');
+
 
 const app = express();
 app.use(express.json());
 app.use('/reservations',reservationRoutes);
-app.use('/users',usuarioRoutes);
+app.use('/users',usersRoutes);
 app.use('/rooms',habitacionRoutes);
+app.use('/auth',authRouter)
+//app.use('/reviews',reviewRoutes)
 
 
 mongoose.connect('mongodb://localhost:27017/HotelPereMaria')
